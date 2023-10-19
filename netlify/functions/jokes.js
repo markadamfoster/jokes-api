22d845de00b3a8e639b9e770d981aff38e1c198f
Mark Foster <foster154@gmail.com>
2023-10-18 21:22:09 -0600

    add public folder

--- netlify/functions/jokes.js
+++ netlify/functions/jokes.js
@@ -10,6 +10,6 @@ exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
   return {
       statusCode: 200,
-      body: `Hello ${subject}!`,
+      body: `Hello Jokes ${subject}!`,
   }
 }

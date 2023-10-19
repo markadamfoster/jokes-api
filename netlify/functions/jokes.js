a605655e59d8af755732162bbbcf82229137d67d
Mark Foster <foster154@gmail.com>
2023-10-18 21:32:04 -0600

    try adding jokes

--- netlify/functions/jokes.js
+++ netlify/functions/jokes.js
@@ -4,12 +4,13 @@
 // I'll get deployed to AWS Lambda, but you don't need to know that. 
 // You can develop and deploy serverless functions right here as part
 // of your site. Netlify Functions will handle the rest for you.
-
+import { jokes } from "../../jokes"
 
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
   return {
       statusCode: 200,
-      body: `Hello Jokes ${subject}!`,
+      body: {
+        allJokes
+      },
   }
 }

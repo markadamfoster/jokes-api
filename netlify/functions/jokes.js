3287253e799e113630217069c96f779ce8b7e007
Mark Foster <foster154@gmail.com>
2023-10-18 21:08:03 -0600

    new function location

--- netlify/functions/jokes.js
+++ netlify/functions/jokes.js
@@ -1,7 +1,7 @@
 import { Config, Context } from "@netlify/functions";
 
 export default async (req, context) => {
-  return new Response(`Hello World!`)
+  return new Response(`Hello World Jokes!`)
 }
 
 export const config = {

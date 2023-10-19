1c0ed2617ecf6bb0852955a7cba342ea9f2f9f5c
Mark Foster <foster154@gmail.com>
2023-10-18 21:50:52 -0600

    add headers

--- netlify/functions/jokes.js
+++ netlify/functions/jokes.js
@@ -115,8 +115,15 @@ exports.handler = async event => {
     return allJokes[num]
   })
 
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  };
+
   return {
       statusCode: 200,
+      headers,
       body: JSON.stringify(jokes),
   }
 }
